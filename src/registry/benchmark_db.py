6d9c1e905a54aac21804446b044e6b0339227703
Vincent Bode <vincent.bode@tum.de>
2024-01-18 18:56:58 -0800

    Add demo benchmark definition

--- src/registry/benchmark_db.py
+++ src/registry/benchmark_db.py
@@ -10,7 +10,9 @@ class BenchmarkDB(BaseRegistry):
     BASE = Benchmark("base", "Baseline", tags=[], duration=timedelta(minutes=60))
     TEST = Benchmark("test", "Test", tags=[], duration=timedelta(minutes=1))
 
+    DEMO = Benchmark("demo", "Demo", tags=[], duration=timedelta(minutes=5))
+
 
 BenchmarkDB.register_all(
-    BenchmarkDB.BASE, BenchmarkDB.TEST
+    BenchmarkDB.BASE, BenchmarkDB.TEST, BenchmarkDB.DEMO,
 )
