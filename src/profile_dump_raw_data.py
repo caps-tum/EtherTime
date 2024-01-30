a98510b8a86602ecf1fc498a6f211151ad6e45a3
Vincent Bode <vincent.bode@tum.de>
2024-01-30 09:40:41 -0800

    Add option to dump raw data from profile

---
+++ src/profile_dump_raw_data.py
@@ -0,0 +1,16 @@
+from argparse import ArgumentParser
+
+from profiles.base_profile import BaseProfile
+from util import StackTraceGuard
+
+if __name__ == '__main__':
+    parser = ArgumentParser("Dump raw data from a profile.")
+    parser.add_argument("profile_path", help="Path of the profile to fetch data from.")
+
+    result = parser.parse_args()
+    with StackTraceGuard():
+        profile = BaseProfile.load(result.profile_path)
+
+        for key, value in profile.raw_data.items():
+            print(f"===== {key} =====")
+            print(value)
