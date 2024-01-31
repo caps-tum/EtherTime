8dd46da380bdc37562c3f8dd1bf32bbe23619f3f
Vincent Bode <vincent.bode@tum.de>
2024-01-31 13:28:13 -0800

    Accurate reading of hardware pulses

--- arduino/pulse_difference_generator/pulse_difference_generator.ino
+++ arduino/pulse_difference_generator/pulse_difference_generator.ino
@@ -1,18 +1,35 @@
+#include <Arduino.h>
 #include <RTCZero.h>
 
 const uint32_t CYCLES_PER_SECOND = 48 * 1000 * 1000; // 48 MHz
 
-const size_t NUM_TRACES = 2;
+const size_t NUM_TRACES = 1;
+
+// Must be a power of 2
 const size_t TRACE_LENGTH = 1024;
+
 typedef struct {
   uint8_t pin = 0;
-  uint8_t lastState = 0;
   size_t index = 0;
-  uint32_t data[TRACE_LENGTH];
+  uint32_t timestamps[TRACE_LENGTH];
+  uint32_t state[TRACE_LENGTH];
 } trace_t;
 
-trace_t traces[NUM_TRACES];
+trace_t trace;
 
+// Calculate the bit mask of register bits we need
+#define PIN_1 6
+#define PIN_2 7
+// const uint32_t GPIO_READ_BITMASK = (1ul << g_APinDescription[PIN_1].ulPin)) | (1ul << g_APinDescription[PIN_2].ulPin))
+const PortGroup* READ_PORT = digitalPinToPort(PIN_1);
+const PortGroup* READ_PORT_2 = digitalPinToPort(PIN_1);
+// _Static_assert(READ_PORT == READ_PORT_2, "Read pin 1 needs to be on the same port as read pin 2");
+const volatile void* READ_REGISTER = portInputRegister(READ_PORT);
+
+#define READ_MASK_PIN_1 digitalPinToBitMask(PIN_1)
+#define READ_MASK_PIN_2 digitalPinToBitMask(PIN_2)
+const uint32_t READ_MASK = READ_MASK_PIN_1 | READ_MASK_PIN_2;
+#define READ_ALL_PINS (*((uint32_t*)READ_REGISTER) & READ_MASK)
 
 RTCZero rtc;
 
@@ -21,76 +38,96 @@ void setup() {
   Serial.begin(9600);
   rtc.begin();
 
-  // Trace mappings
-  traces[0].pin = 6;
-  traces[1].pin = 7;
 
-  // Initialize traces
-  for(size_t i = 0; i < NUM_TRACES; i++) {
-    traces[i].index = 0;
-    pinMode(traces[i].pin, INPUT_PULLUP);
-  }
+  pinMode(PIN_1, INPUT_PULLUP);
+  pinMode(PIN_2, INPUT_PULLUP);
 
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+void printTrace(unsigned int traceNumber, uint32_t bitmask) {
+  Serial.print("Trace ");
+  Serial.println(traceNumber);
+
+  for(size_t index = 1; index < trace.index; index++) {
+    // Check if the bit in question changed (important: start at index 1 to prevent underflow).
+    if((trace.state[index - 1] & bitmask)
+       != (trace.state[index] & bitmask)) {
+      Serial.print(trace.timestamps[index]);
+      Serial.print(" ");
+    }
+  }
+  Serial.println("");
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
 
-  for(int i = 0; i < 10; i++) {
+  for(int i = 0; i < 30; i++) {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
     digitalWrite(LED_BUILTIN, LOW);
     delay(100);
   }
 
-  Serial.print("Begin cycle benchmark. ");
-
-  // Reset traces
-  for(size_t i = 0; i < NUM_TRACES; i++) {
-    traces[i].index = 0;
-  }
-
 //  rtc.setDate(1970, 1, 1);
   rtc.setTime(0, 0, 0);
 
-  uint32_t counter = 0;
-  for(; counter < 10 * 1000000;) {
-    counter++;
+  Serial.println("Begin cycle benchmark.");
+  Serial.flush();
 
-    for(size_t trace = 0; trace < NUM_TRACES; trace++) {
-      bool result = digitalRead(traces[trace].pin);
-      if(result != traces[trace].lastState) {
-        traces[trace].lastState = result;
-        digitalWrite(LED_BUILTIN, result);
-        traces[trace].data[traces[trace].index] = counter;
-        traces[trace].index++;
-        if(traces[trace].index >= TRACE_LENGTH - 1) {
-          traces[trace].index = TRACE_LENGTH - 1; 
-        }
-      }
-      // delay(1);
-      delayMicroseconds(1);
+
+  trace.index = 0;
+  trace.state[trace.index] = READ_ALL_PINS;
+
+  noInterrupts();
+  uint32_t cycles = 0;
+  uint32_t start = micros();
+  // for(; cycles < 1 * 1000000;) {
+  //   cycles++;
+  // uint32_t flanksRecorded = 0;
+  while(trace.index < TRACE_LENGTH - 1) {
+
+    // Read the pins, all at once using a register.
+    // bool result = digitalRead(traces[trace].pin);
+    uint32_t result = READ_ALL_PINS;
+
+    if(result != trace.state[trace.index]) {
+      // And the index with the trace length - 1, preventing the index from exceeding buffer size.
+      trace.index = (trace.index + 1) & (TRACE_LENGTH - 1);
+
+      // Methods to read the time: number of loop cycles, using a register, or the arduino function
+      // traces[trace].data[index] = cycles;
+      trace.timestamps[trace.index] = SysTick->VAL;
+      // traces[trace].data[index] = micros();
+
+      trace.state[trace.index] = result;
+      //digitalWrite(LED_BUILTIN, result);
+
+      // flanksRecorded++;
+      // if(flanksRecorded >= TRACE_LENGTH - 1) {
+      //   break;
+      // }
     }
-    
+    // delay(1);
+    //delayMicroseconds(1);
   }
+  interrupts();
 
+  uint32_t stop = micros();
   uint32_t final_time = rtc.getSeconds();
 
   Serial.print("Finished: ");
   Serial.print(final_time);
   Serial.print(" seconds = ");
-  Serial.print(counter);
-  Serial.print(" cycles.\n");
+  Serial.print(stop - start);
+  Serial.print(" us, cycles = ");
+  Serial.print(cycles);
+  Serial.print("\n");
+  Serial.flush();
 
-  for(size_t trace = 0; trace < NUM_TRACES; trace++) {
-    Serial.print("Trace ");
-    Serial.println(trace);
-    for(size_t index = 0; index < traces[trace].index; index++) {
-      Serial.print(traces[trace].data[index]);
-      Serial.print(" ");
-    }
-    Serial.println("");
-  }
+  printTrace(0, READ_MASK_PIN_1);
+  printTrace(1, READ_MASK_PIN_2);
+
 }
