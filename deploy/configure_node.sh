8b54e3bd8c087be3a3cf125bce0ee77fab20e1a9
Vincent Bode <vincent.bode@tum.de>
2024-02-07 14:38:31 -0800

    Update dependency list

--- deploy/configure_node.sh
+++ deploy/configure_node.sh
@@ -3,4 +3,8 @@
 set -x
 
 sudo apt install -y tmux iperf stress-ng ptpd linuxptp || exit 255
-pip3 install --user --break-system-packages rpyc pydantic || exit 255
+pip3 install --user --break-system-packages rpyc pydantic pandas matplotlib || exit 255
+
+echo "Don't forget to disable the desktop environment (e.g. using raspi-config)"
+
+echo "Copy over the PPSi library as well."
