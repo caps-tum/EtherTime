269d9cc0bd060cc6221f8698a7f97356eecdcc24
Vincent Bode <vincent.bode@tum.de>
2024-02-12 09:49:03 -0800

    Static configuration of ip addresses in config

--- deploy/configure_node.sh
+++ deploy/configure_node.sh
@@ -7,4 +7,12 @@ pip3 install --user --break-system-packages rpyc pydantic pandas matplotlib || e
 
 echo "Don't forget to disable the desktop environment (e.g. using raspi-config)"
 
+echo "Add the following to a file in /etc/network/interfaces.d/benchmark_interface.conf:
+auto eth0
+iface eth0 inet static
+  address 10.0.0.<IP>
+  netmask 255.255.255.0
+  gateway 10.0.0.1
+"
+
 echo "Copy over the PPSi library as well."
