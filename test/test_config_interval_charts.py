ae65bd7be50e2be080c5aafee84610fb81b2dfa8
Vincent Bode <vincent.bode@tum.de>
2024-02-14 09:15:25 -0800

    Config interval charts

--- test/test_config_interval_charts.py
+++ test/test_config_interval_charts.py
@@ -1,3 +1,4 @@
+import math
 from unittest import TestCase
 
 import constants
@@ -9,7 +10,11 @@ from registry.resolve import ProfileDB
 CONFIG_CHART_DIRECTORY = constants.CHARTS_DIR.joinpath("config")
 
 
-class TestLoadCharts(TestCase):
+class TestConfigurationCharts(TestCase):
+    @staticmethod
+    def sync_interval_to_syncs_per_second(interval: float):
+        return 1 / math.pow(2, interval)
+
     def test_create(self):
         profile_db = ProfileDB()
         profiles = profile_db.resolve_all(
@@ -21,15 +26,15 @@ class TestLoadCharts(TestCase):
 
         chart = ComparisonChart("Time Sampling Interval", profiles, nrows=2)
         chart.plot_median_clock_diff_and_path_delay(
-            lambda profile: profile.benchmark.ptp_config.log_sync_interval,
-            x_axis_label="PTP Sync & Delay Request Interval (log scale)",
+            lambda profile: self.sync_interval_to_syncs_per_second(profile.benchmark.ptp_config.log_sync_interval),
+            x_axis_label="PTP Sync & Delay Request Frequency (1/s)",
         )
         chart.save(CONFIG_CHART_DIRECTORY.joinpath("config_interval.png"), make_parent=True)
 
         chart = ComparisonChart("Unisolated Network Load", profiles, nrows=2)
         chart.plot_median_clock_diff_and_path_delay(
-            lambda profile: profile.benchmark.ptp_config.log_sync_interval,
-            x_axis_label="PTP Sync & Delay Request Interval (log scale)",
+            lambda profile: self.sync_interval_to_syncs_per_second(profile.benchmark.ptp_config.log_sync_interval),
+            x_axis_label="PTP Sync & Delay Request Frequency (1/s)",
             include_p99=True,
         )
         chart.save(CONFIG_CHART_DIRECTORY.joinpath("config_interval_p99.png"), make_parent=True)
