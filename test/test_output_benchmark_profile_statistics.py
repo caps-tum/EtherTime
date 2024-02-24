dc4fc51d1105c68f9ae5ce055dac98d44636267c
Vincent Bode <vincent.bode@tum.de>
2024-02-23 18:46:38 -0800

    Better profile count outputs

--- test/test_output_benchmark_profile_statistics.py
+++ test/test_output_benchmark_profile_statistics.py
@@ -1,9 +1,13 @@
 
 from unittest import TestCase
 
+import pandas
 import pandas as pd
 from natsort import natsorted, natsort_keygen
+
+from constants import CHARTS_DIR
 from registry import resolve
+from registry.benchmark_db import BenchmarkDB
 from registry.resolve import ProfileDB
 
 
@@ -20,11 +24,22 @@ class TestOutputBenchmarkProfileStatistics(TestCase):
 
         frame = pd.DataFrame(records)
 
-        benchmarks_by_vendor = frame.groupby(by=["Benchmark", "Vendor"]).count().unstack("Vendor")
+        benchmarks_by_vendor: pd.DataFrame = frame.groupby(by=["Benchmark", "Vendor"]).count().unstack("Vendor")
         # Discard one level of multiindex
         # benchmarks_by_vendor.columns = benchmarks_by_vendor.columns.to_flat_index()
         benchmarks_by_vendor.columns = benchmarks_by_vendor.columns.get_level_values(-1)
         benchmarks_by_vendor = benchmarks_by_vendor.rename_axis(None, axis=1).reset_index().sort_values(by="Benchmark", key=natsort_keygen()).reset_index(drop=True)
-        print(benchmarks_by_vendor)
+
+        # Add missing records
+        benchmarks = pd.DataFrame([{"Benchmark": benchmark.name} for benchmark in BenchmarkDB.all()])
+        benchmarks_by_vendor = pd.merge(
+            benchmarks, benchmarks_by_vendor,
+            on="Benchmark",
+            how="outer",
+        )
+
+        output = benchmarks_by_vendor.to_string()
+        CHARTS_DIR.joinpath("profile_stats.txt").write_text(output)
+        print(output)
         # Avoid overlap with stderr
         print("")
