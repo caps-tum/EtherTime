16e228aab5ef222cf971b7173cbfe7271e3a159a
Vincent Bode <vincent.bode@tum.de>
2024-02-26 19:28:35 -0800

    Fix sorting in statistics output due to upgraded pandas

--- test/test_output_benchmark_profile_statistics.py
+++ test/test_output_benchmark_profile_statistics.py
@@ -28,7 +28,7 @@ class TestOutputBenchmarkProfileStatistics(TestCase):
         # Discard one level of multiindex
         # benchmarks_by_vendor.columns = benchmarks_by_vendor.columns.to_flat_index()
         benchmarks_by_vendor.columns = benchmarks_by_vendor.columns.get_level_values(-1)
-        benchmarks_by_vendor = benchmarks_by_vendor.rename_axis(None, axis=1).reset_index().sort_values(by="Benchmark", key=natsort_keygen()).reset_index(drop=True)
+        benchmarks_by_vendor = benchmarks_by_vendor.rename_axis(None, axis=1).reset_index()
 
         # Add missing records
         benchmarks = pd.DataFrame([{"Benchmark": benchmark.name} for benchmark in BenchmarkDB.all()])
@@ -37,6 +37,9 @@ class TestOutputBenchmarkProfileStatistics(TestCase):
             on="Benchmark",
             how="outer",
         )
+
+        # Sort
+        benchmarks_by_vendor = benchmarks_by_vendor.sort_values(by="Benchmark", key=natsort_keygen()).reset_index(drop=True)
 
         output = benchmarks_by_vendor.to_string()
         CHARTS_DIR.joinpath("profile_stats.txt").write_text(output)
