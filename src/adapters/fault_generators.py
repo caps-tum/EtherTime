d14c427f305e476b763fddd74ef8441dff7232b0
Vincent Bode <vincent.bode@tum.de>
2024-02-29 14:28:10 -0800

    Software fault: avoid drifting

--- src/adapters/fault_generators.py
+++ src/adapters/fault_generators.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import timedelta, datetime
 
 from adapters.adapter import Adapter
 
@@ -12,8 +13,11 @@ class SoftwareFaultGenerator(Adapter):
 
         # We do this until we are cancelled
         self.log(f"Scheduling software faults every {interval} on {self.profile.configuration.machine.id}")
+        # Don't drift
+        next_wakeup = datetime.now() + interval
         while True:
-            await asyncio.sleep(interval.total_seconds())
+            await asyncio.sleep((next_wakeup - datetime.now()).total_seconds())
             if vendor.running:
                 self.log(f"Scheduled software fault imminent on {self.profile.configuration.machine}.")
                 await vendor.restart(kill=True)
+            next_wakeup += interval
