76dd8a00d966b9697060207b22417554b36cfa70
Vincent Bode <vincent.bode@tum.de>
2024-03-05 10:40:15 -0800

    Switch db to postgres

--- ptp_perf/django/site/settings.py
+++ ptp_perf/django/site/settings.py
@@ -75,9 +75,16 @@ WSGI_APPLICATION = 'ptp_perf.django.site.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '192.168.1.100',
+        'NAME': 'ptpperf',
+        'USER': 'ptpperf',
+        'PASSWORD': 'xOPI2EcpTUAY1srH',
     }
 }
 
