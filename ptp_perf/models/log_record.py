7bcb130aee705e24d73d20275fce0624a826470a
Vincent Bode <vincent.bode@tum.de>
2024-03-07 13:58:40 -0800

    Remove auto_now on log_record

--- ptp_perf/models/log_record.py
+++ ptp_perf/models/log_record.py
@@ -5,7 +5,7 @@ from ptp_perf.models import PTPEndpoint
 
 class LogRecord(models.Model):
     id = models.AutoField(primary_key=True)
-    timestamp = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField()
     endpoint = models.ForeignKey(PTPEndpoint, on_delete=models.CASCADE)
     source = models.CharField(max_length=255, null=False, blank=False)
 
