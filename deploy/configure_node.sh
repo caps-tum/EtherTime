fc472ba653ab62397bdb0565de138b4a45b38138
Vincent Bode <vincent.bode@tum.de>
2024-04-09 18:15:31 -0700

    Fix some rpi-5 configuration issues

--- deploy/configure_node.sh
+++ deploy/configure_node.sh
@@ -11,16 +11,22 @@ sudo su rpi bash -c "umask 077 && mkdir -p ~/.ssh && echo 'ecdsa-sha2-nistp256 A
 
 echo "Don't forget to disable the desktop environment (e.g. using raspi-config)"
 
-echo "Add the following to a file in nano /etc/network/interfaces.d/benchmark_interface.conf:
-auto eth0
-iface eth0 inet static
-  address 10.0.0.<IP>
-  netmask 255.255.255.0
-  gateway 10.0.0.1
-"
+
+echo "Use sudo nmtui to configure the wired interface"
+#echo "Add the following to a file in nano /etc/network/interfaces.d/benchmark_interface.conf:
+#auto eth0
+#iface eth0 inet static
+#  address 10.0.0.<IP>
+#  netmask 255.255.255.0
+#  gateway 10.0.0.1
+#"
 
 echo "Copy over the PPSi and SPTP libraries as well."
 # Run this as RPI
 mkdir -p ~/ptp-perf
-rsync -av 'rpi@192.168.1.106:~/ptp-perf/lib' '/home/rpi/ptp-perf/lib'
-rsync -av 'rpi@192.168.1.106:~/go' '/home/rpi/go'
+#rsync -av 'rpi@192.168.1.106:~/ptp-perf/lib' '/home/rpi/ptp-perf/'
+#rsync -av 'rpi@192.168.1.106:~/go' '/home/rpi/'
+
+# sudo rm -rf /home/rpi/go /home/rpi/ptp-perf/lib
+rsync -av 'rpi@10.0.0.6:~/ptp-perf/lib' '/home/rpi/ptp-perf/'
+rsync -av 'rpi@10.0.0.6:~/go' '/home/rpi/'
