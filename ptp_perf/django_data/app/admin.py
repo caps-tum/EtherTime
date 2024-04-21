67c146af14122164f2eaa0eb25217fdcc505752c
Vincent Bode <vincent.bode@tum.de>
2024-04-21 11:09:15 -0700

    More admin quality of life

--- ptp_perf/django_data/app/admin.py
+++ ptp_perf/django_data/app/admin.py
@@ -5,11 +5,12 @@ from urllib.parse import urlencode
 from admin_actions.admin import ActionsModelAdmin
 from django.contrib import admin
 from django.db import transaction
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 
 from ptp_perf.models import PTPProfile, PTPEndpoint, LogRecord, Sample, Tag, ScheduleTask, BenchmarkSummary
+from ptp_perf.models.endpoint_type import EndpointType
 from ptp_perf.registry.benchmark_db import BenchmarkDB
 from ptp_perf.test.test_key_metric_variance_charts import KeyMetricVarianceCharts
 from ptp_perf.util import unpack_one_value
@@ -71,13 +72,22 @@ class PTPProfileAdmin(ActionsModelAdmin):
                    'is_corrupted')
     # inlines = [PTPEndpointInline]
     actions = (delete_analysis_output,)
-    actions_row = ('create_timeseries_for_profile',)
+    actions_row = ('get_endpoints', 'create_timeseries_for_profile',)
 
     def create_timeseries_for_profile(self, request, pk):
         profile = PTPProfile.objects.get(pk=pk)
         return render_timeseries_to_http_response(profile.endpoint_primary_slave)
+
     create_timeseries_for_profile.short_description = 'Timeseries'
     create_timeseries_for_profile.url_path = 'profile_timeseries'
+
+    def get_endpoints(self, request, pk):
+        profile = PTPProfile.objects.get(pk=pk)
+        return HttpResponseRedirect(
+            get_endpoint_admin_link(profile.benchmark_id, profile.vendor_id, profile.cluster_id, profile.id)
+        )
+    get_endpoints.short_description = 'Endpoints'
+    get_endpoints.url_path = 'endpoints'
 
 
 @admin.register(PTPEndpoint)
@@ -88,7 +98,7 @@ class PTPEndpointAdmin(ActionsModelAdmin):
     list_select_related = ('profile',)
     list_filter = ('endpoint_type', 'profile__benchmark_id', 'profile__vendor_id', 'profile__cluster_id')
     actions = (create_key_metric_variance_chart,)
-    actions_row = ('create_timeseries', )
+    actions_row = ('create_timeseries',)
 
     def benchmark(self, endpoint: PTPEndpoint):
         return endpoint.profile.benchmark.name
@@ -106,11 +116,13 @@ class PTPEndpointAdmin(ActionsModelAdmin):
 
     def clock_diff_median_formatted(self, endpoint: PTPEndpoint):
         return format_time_offset(endpoint.clock_diff_median, auto_increase_places=True)
+
     clock_diff_median_formatted.admin_order_field = 'clock_diff_median'
     clock_diff_median_formatted.short_description = 'Clock Diff Median'
 
     def clock_diff_p95_formatted(self, endpoint: PTPEndpoint):
         return format_time_offset(endpoint.clock_diff_p95, auto_increase_places=True)
+
     clock_diff_p95_formatted.admin_order_field = 'clock_diff_p95'
     clock_diff_p95_formatted.short_description = 'Clock Diff 95%'
 
@@ -123,9 +135,9 @@ class PTPEndpointAdmin(ActionsModelAdmin):
     def create_timeseries(self, request, pk):
         endpoint: PTPEndpoint = PTPEndpoint.objects.get(pk=pk)
         return render_timeseries_to_http_response(endpoint)
+
     create_timeseries.short_description = 'Timeseries'
     create_timeseries.url_path = 'timeseries'
-
 
 
 @admin.register(LogRecord)
@@ -162,12 +174,27 @@ class ScheduleTaskAdmin(admin.ModelAdmin):
     actions = [toggle_pause]
 
 
+def get_endpoint_admin_link(benchmark_id, vendor_id, cluster_id, profile_id: int = None,
+                            endpoint_type: EndpointType = None):
+    filters = {
+        'profile__benchmark_id': benchmark_id,
+        'profile__cluster_id': cluster_id,
+        'profile__vendor_id': vendor_id,
+    }
+    if endpoint_type is not None:
+        filters['endpoint_type__exact'] = endpoint_type.value
+    if profile_id is not None:
+        filters['profile__id'] = profile_id
+
+    return reverse_lazy('admin:app_ptpendpoint_changelist') + '?' + urlencode(filters)
+
+
 @admin.register(BenchmarkSummary)
 class BenchmarkSummaryAdmin(ActionsModelAdmin):
     list_display = ('id', 'benchmark_id', 'vendor_id', 'cluster_id', 'count', 'clock_diff_median_formatted',
                     'vs_baseline', 'clock_diff_p95_formatted', 'p95_vs_baseline')
     list_filter = ('benchmark_id', 'vendor_id', 'cluster_id')
-    actions_row = ('details', )
+    actions_row = ('details',)
 
     def clock_diff_median_formatted(self, summary: BenchmarkSummary):
         return format_time_offset(summary.clock_diff_median, auto_increase_places=True)
@@ -196,13 +223,8 @@ class BenchmarkSummaryAdmin(ActionsModelAdmin):
     def details(self, request, pk):
         summary = BenchmarkSummary.objects.get(pk=pk)
         return redirect(
-            reverse_lazy('admin:app_ptpendpoint_changelist')
-            + '?' + urlencode({
-                'endpoint_type__exact': 'PRIMARY_SLAVE',
-                'profile__benchmark_id': summary.benchmark_id,
-                'profile__cluster_id': summary.cluster_id,
-                'profile__vendor_id': summary.vendor_id,
-            })
+            get_endpoint_admin_link(summary.benchmark_id, summary.vendor_id, summary.cluster_id,
+                                    endpoint_type=EndpointType.PRIMARY_SLAVE)
         )
     details.short_description = 'Details'
     details.url_path = 'details'
