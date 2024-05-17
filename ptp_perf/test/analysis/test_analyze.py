8a9803b1cb606dc1773be853412099f0c6e73730
Vincent Bode <vincent.bode@tum.de>
2024-05-16 21:55:41 -0700

    Show logs in debug analysis

--- ptp_perf/test/analysis/test_analyze.py
+++ ptp_perf/test/analysis/test_analyze.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 
 from ptp_perf.django_data.app.management.commands.analyze import run_analysis
+from ptp_perf.util import setup_logging
 
 
 class TestAnalyze(TestCase):
 
     def test_analyze(self):
+        setup_logging()
         run_analysis(False)
